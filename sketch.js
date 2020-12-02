8286fe936e2974dd3dc053a6b432d3b56ada45be
richard2006123 <75129594+richard2006123@users.noreply.github.com>
2020-12-02 20:01:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var fixed ,moving;
+function setup() {
+  createCanvas(1200,800);
+  fixed=createSprite(600, 200, 50, 80);
+  fixed.shapeColor="green";
+  fixed.debug=true;
+  moving=createSprite(400,200,80,30);
+  moving.shapeColor="green";
+  moving.debug=true;
+
+}
+
+function draw() {
+  background("black");  
+  moving.x=World.mouseX;
+  moving.y=World.mouseY;
+  if (moving.x-fixed.x<fixed.width/2+moving.width /2&& fixed.x-moving.x<fixed.width/2+moving.width/2
+    && moving.y-fixed.y<fixed.height/2+moving.height /2&& fixed.y-moving.y<fixed.height/2+moving.height/2){
+    moving.shapeColor="red";
+    fixed.shapeColor="red";
+  }
+  else {
+    moving.shapeColor="green";
+    fixed.shapeColor="green";
+  }
+  drawSprites();
+}
